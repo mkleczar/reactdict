c36b057e30207491c5a5ef993bdd6d889e6983f4
Mariusz Kleczar <kleczar.mariusz@gmail.com>
2021-12-08 22:46:34 +0100

    clean some code

--- src/test/java/pl/example/reactdict/reporsitory/impl/WordStreamDictionaryRepositoryTest.java
+++ src/test/java/pl/example/reactdict/reporsitory/impl/WordStreamDictionaryRepositoryTest.java
@@ -6,8 +6,6 @@ import pl.example.reactdict.repository.DictionaryRepository;
 import pl.example.reactdict.repository.impl.FromFileWordStreamService;
 import pl.example.reactdict.repository.impl.WordStreamDictionaryRepository;
 import reactor.core.publisher.Flux;
-
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,8 +17,7 @@ public class WordStreamDictionaryRepositoryTest {
     public void testFindingWordsByRegex() {
         DictionaryRepository repo = new WordStreamDictionaryRepository(new FromFileWordStreamService());
         Flux<String> result = repo.find("d.*ariusz.*");
-        List<String> list = new ArrayList<>();
-        result.subscribe(list::add);
+        List<String> list = result.collectList().block();
         assertThat(list)
                 .hasSize(84)
                 .contains("diariusz", "depozytariusz");
