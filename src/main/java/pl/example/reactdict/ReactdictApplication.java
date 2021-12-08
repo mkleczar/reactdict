48dae7747b52fb06b13c079abbccfa120eaed3f2
Mariusz Kleczar <kleczar.mariusz@gmail.com>
2021-12-08 10:02:28 +0100

    initial revision

---
+++ src/main/java/pl/example/reactdict/ReactdictApplication.java
@@ -0,0 +1,13 @@
+package pl.example.reactdict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactdictApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactdictApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/example/reactdict/ReactdictApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.example.reactdict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactdictApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
